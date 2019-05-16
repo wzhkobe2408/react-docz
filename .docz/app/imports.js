692ef788b5d9a67c45d42b1617b6475a668f3b13
王众欢 <wangzhonghuan@bytedance.com>
2019-05-16 23:04:47 +0800

    second commit

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,12 +1,12 @@
 export const imports = {
-  'docs/Button.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-button" */ 'docs/Button.mdx'),
-  'docs/Image.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-image" */ 'docs/Image.mdx'),
-  'docs/Contributors.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-contributors" */ 'docs/Contributors.mdx'),
-  'docs/Input.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-input" */ 'docs/Input.mdx'),
-  'docs/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-index" */ 'docs/index.mdx'),
-}
+  "docs/Button.mdx": () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-button" */ "docs/Button.mdx"),
+  "docs/Contributors.mdx": () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-contributors" */ "docs/Contributors.mdx"),
+  "docs/Image.mdx": () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-image" */ "docs/Image.mdx"),
+  "docs/Input.mdx": () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-input" */ "docs/Input.mdx"),
+  "docs/index.mdx": () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-index" */ "docs/index.mdx")
+};
--- .docz/app/root.jsx
+++ .docz/app/root.jsx
@@ -1,22 +1,15 @@
-import React from 'react'
-import { setConfig, hot } from 'react-hot-loader'
-import Theme from 'docz-theme-default'
+import React from "react";
+import { setConfig, hot } from "react-hot-loader";
+import Theme from "docz-theme-default";
 
-import { imports } from './imports'
-import db from './db.json'
+import { imports } from "./imports";
+import db from "./db.json";
 
-const Root = () => (
-  <Theme
-    db={db}
-    imports={imports}
-    hashRouter={false}
-    websocketUrl="ws://127.0.0.1:60505"
-  />
-)
+const Root = () => <Theme db={db} imports={imports} hashRouter={false} />;
 
 // TODO: this is temporary until react-hot-loader fix hooks issues
 setConfig({
-  pureSFC: true,
-})
+  pureSFC: true
+});
 
-export default hot(module)(Root)
+export default hot(module)(Root);
